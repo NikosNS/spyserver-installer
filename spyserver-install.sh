3685b55f284810ef2ab2f0013c4933712d26b3f3
NikosNS <sheva94.ss@gmail.com>
2023-04-12 12:31:57 +0300

    new script added

--- spyserver-install.sh
+++ spyserver-install.sh
@@ -1,42 +1,97 @@
-#!/bin/bash 
+#!/bin/bash
 
-# by NikosNS
-cd ~ 
-apt update && apt upgrade -y && apt install -y airspy libairspy0 libairspy-dev rtl-sdr librtlsdr-dev
-apt install -y wget htop tmux iftop
- 
-# check if  there is spyserver directory from previous installaltion 
-if [ -d ~/spyserver ]
+# simple function to wait few seconds
+timeout () {
+  for (( i=1; i<=7; i++ ))
+  do
+    printf "\033[0;31m.\n\033[0m"
+    for j in $(seq 1 $i)
+    do
+      printf "\033[0;31m.\033[0m"
+      sleep 1
+    done
+  done
+}
+
+
+# cheeck if user is root
+if [ `whoami` != 'root' ]
 then
-  rm -rf ~/spyserver
+  printf "\033[0;31mNeed root, try \033[0;32msudo -i\033[0m \033[0;31mbefore start\n\033[0m"
+  exit 0
 fi
 
-mkdir spyserver && cd spyserver 
-# there programm will have tryed to find app do with the architecture 
-if [ `uname -m` == "armhf" ] || [ `uname -m` == "armv7l" ]
-then 
-  link="https://www.airspy.com/?ddownload=4247" 
-elif [ `uname -m` == "aarch64" ]
-then 
-  link="https://www.airspy.com/?ddownload=5795"
-elif [ `uname -m` == "x86_64" ]
+# upgrade and install all libraries
+apt update && apt upgrade -y && apt install -y rtl-sdr librtlsdr-dev librtlsdr0 wget htop tmux git iftop
+
+# check if there is not first attempt to install
+if [ -d /home/pi/spyserver ]
 then
-  link="https://www.airspy.com/?ddownload=4262" 
-elif [ `uname -m` == "x86" ]
+  echo "already installed"
+  rm -rf /home/pi/spyserver*
+fi
+cd /home/pi && mkdir spyserver && cd spyserver
+
+# download the spyservers archieve from oficial site, depends on architecture
+if [ `uname -m` == 'aarch64' ]
 then
-  link="https://www.airspy.com/?ddownload=4308"
+  wget -O spyserver.tgz https://www.airspy.com/?ddownload=5795
 else
-  echo "There is an unknown architecture"
+  wget -O spyserver.tgz https://www.airspy.com/?ddownload=4247
 fi
-  
-wget -O spyserver.tgz $link 
-tar xvzf spyserver.tgz && rm -rf spyserver.tgz 
-# there programm wants to know some specific parameters  
-echo "Print the IP address for listening (default is 0.0.0.0), for example 192.168.1.0/24 or 8.8.8.8\n" 
-read ip 
-echo "Print the port (default is between 5555 and 6666)\n" 
-read port 
-echo "Print the type of device (for example RTL-SDR)\n" 
-read device 
-sed -e 's/bind_host = 0.0.0.0/bind_host = $ip/g' -e '/bind_port = 5555-6666/bind_port = $port/g' -e '/device_type = auto/device_type = $device/g' > spyserver.config 
- 
+tar xvzf spyserver.tgz && rm *.tgz
+
+# editing configfile, then moving files to /usr/bin and /etc
+printf "You can add some new parameters in configfile via Nano editor after few sec\n"
+timeout
+nano spyserver.config
+cp spyserver /usr/bin && cp spyserver.config /etc
+chown -R pi:pi /home/pi/spyserver
+
+# creating the service file, then creating aliases,
+# and crontab dependencies based by ping,
+# also I download rtl-sdr.rules with right
+# permissions to all users in all groups
+cat <<EOF > /etc/systemd/system/spyserver.service
+[Unit]
+Description=spyserver always running service
+After=network.target
+StartLimitBurst=5
+StartLimitIntervalSec=10
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5
+User=pi
+ExecStart=/usr/bin/spyserver /etc/spyserver.config
+
+[Install]
+WantedBy=multi-user.target
+EOF
+cat <<EOF >>/home/pi/.bashrc
+alias sstart='sudo systemctl start spyserver.service'
+alias sstop='sudo systemctl stop spyserver.service'
+EOF
+wget https://raw.githubusercontent.com/keenerd/rtl-sdr/master/rtl-sdr.rules && mv rtl-sdr.rules /etc/udev/rules.d/
+read -p "Print 2 IP to bind service activity by ping them \n Example 8.8.8.8 1.1.1.1:  " ip1 ip2
+cat <<EOF >> /etc/crontab
+*/3 * * * * root /bin/bash/ -c "ping -c 3 $ip1 >/dev/null || ping -c 3 $ip2 >/dev/null; if [ $? != 0 ]; then systemctl stop spyserver.service && echo 'PING NOT OK' `date` >> /var/log/pingerr.log; else if [ systemctl is-active spyserver.service == 'inactive' ]; then systemctl daemon-reload && systemctl start spyserver.service; fi; fi"
+EOF
+systemctl enable spyserver.service && systemctl daemon-reload && systemctl start spyserver.service
+
+# additional info and condition for reboot the system
+printf "\n"
+timeout
+printf "\nTo stop or start service print \033[0;32msstart\033[0m or \033[0;32msstart\033[0m\nOr do it throught \033[0;32msystemctl\n\033[0m"
+printf "System needs reboot, chose the option and press enter,\t y/N "
+read yn
+if [ $yn == 'y' ]
+then
+  printf "\033[0;31mreboot now\n\033[0m"
+  timeout
+  reboot now
+else
+  printf "Restart your system mannualy with \033[0;32msudo -i\033[0m \n"
+  exit 0
+fi
